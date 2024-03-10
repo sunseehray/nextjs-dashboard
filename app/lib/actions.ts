a497977c3573a91fed5e4f69cf49f42503608300
sunseehray <sabtirazona@byui.edu>
2024-03-10 16:54:27 +0800

    yehey, edit-form also done with accessibility.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -48,11 +48,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       }
 
     // prepare data for insertion into the database
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+    const { customerId, amount, status } = validatedFields.data;
 
     // insert data into database
     const amountInCents = amount * 100;
@@ -73,13 +69,26 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    // Validate form fields using Zod
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    // if form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    // prepare data for updating into the database
+    const { customerId, amount, status } = validatedFields.data;
+
+    // insert data into database
     const amountInCents = amount * 100;
 
     try {
